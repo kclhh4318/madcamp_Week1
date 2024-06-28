c85d8389ff20b001c975c546a607ef1242bd5986
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-06-28 18:48:33 +0900

    문자 메시지 창 띄우는 작업

--- app/src/main/java/com/example/myapplication/Utils.kt
+++ app/src/main/java/com/example/myapplication/Utils.kt
@@ -17,28 +17,28 @@ data class SongInfo(
 // 계절별 음악 리스트
 val seasonMusic = mapOf(
     "봄" to listOf(
-        Pair("spring_music_1", "spr1"),
-        Pair("spring_music_2", "spr2"),
-        Pair("spring_music_3", "spr3"),
-        Pair("spring_music_4", "spr4"),
-        Pair("spring_music_5", "spr5"),
-        Pair("spring_music_6", "spr6")
+        Pair("sprs1", "spr1"),
+        Pair("sprs2", "spr2"),
+        Pair("sprs3", "spr3"),
+        Pair("sprs4", "spr4"),
+        Pair("sprs5", "spr5"),
+        Pair("sprs6", "spr6")
     ),
     "여름" to listOf(
-        Pair("summer_music_1", "sum1"),
-        Pair("summer_music_2", "sum2"),
-        Pair("summer_music_3", "sum3"),
-        Pair("summer_music_4", "sum4"),
-        Pair("summer_music_5", "sum5"),
-        Pair("summer_music_6", "sum6")
+        Pair("sms1", "sum1"),
+        Pair("sms2", "sum2"),
+        Pair("sms3", "sum3"),
+        Pair("sms4", "sum4"),
+        Pair("sms5", "sum5"),
+        Pair("sms6", "sum6")
     ),
     "가을" to listOf(
-        Pair("autumn_music_1", "aut1"),
-        Pair("autumn_music_2", "aut2"),
-        Pair("autumn_music_3", "aut3"),
-        Pair("autumn_music_4", "aut4"),
-        Pair("autumn_music_5", "aut5"),
-        Pair("autumn_music_6", "aut6")
+        Pair("ats1", "aut1"),
+        Pair("ats2", "aut2"),
+        Pair("ats3", "aut3"),
+        Pair("ats4", "aut4"),
+        Pair("ats5", "aut5"),
+        Pair("ats6", "aut6")
     ),
     "겨울" to listOf(
         Pair("winter_music_1", "win1"),
@@ -51,23 +51,23 @@ val seasonMusic = mapOf(
 )
 
 val songInfos = mapOf(
-    "spring_music_1" to SongInfo("Spring Song 1", "Artist A", 2020),
-    "spring_music_2" to SongInfo("Spring Song 2", "Artist B", 2021),
-    "spring_music_3" to SongInfo("Spring Song 3", "Artist C", 2022),
-    "spring_music_4" to SongInfo("Spring Song 4", "Artist D", 2023),
-    "spring_music_5" to SongInfo("Spring Song 5", "Artist E", 2024),
-    "spring_music_6" to SongInfo("Spring Song 6", "Artist F", 2025),
+    "sprs1" to SongInfo("Spring Song 1", "Artist A", 2020),
+    "sprs2" to SongInfo("Spring Song 2", "Artist B", 2021),
+    "sprs3" to SongInfo("Spring Song 3", "Artist C", 2022),
+    "sprs_4" to SongInfo("Spring Song 4", "Artist D", 2023),
+    "sprs5" to SongInfo("Spring Song 5", "Artist E", 2024),
+    "sprs6" to SongInfo("Spring Song 6", "Artist F", 2025),
     "summer_music_1" to SongInfo("Summer Song 1", "Artist G", 2020),
     "summer_music_2" to SongInfo("Summer Song 2", "Artist G", 2020),
     "summer_music_3" to SongInfo("Summer Song 3", "Artist G", 2020),
     "summer_music_4" to SongInfo("Summer Song 4", "Artist G", 2020),
     "summer_music_5" to SongInfo("Summer Song 5", "Artist G", 2020),
     "summer_music_6" to SongInfo("Summer Song 6", "Artist G", 2020),
-    "autumn_music_1" to SongInfo("Autumn Song 1", "Artist H", 2020),
-    "autumn_music_2" to SongInfo("Autumn Song 2", "Artist H", 2020),
-    "autumn_music_3" to SongInfo("Autumn Song 3", "Artist H", 2020),
-    "autumn_music_4" to SongInfo("Autumn Song 4", "Artist H", 2020),
-    "autumn_music_5" to SongInfo("Autumn Song 5", "Artist H", 2020),
+    "ats1" to SongInfo("Autumn Song 1", "Artist H", 2020),
+    "ats2" to SongInfo("Autumn Song 2", "Artist H", 2020),
+    "ats3" to SongInfo("Autumn Song 3", "Artist H", 2020),
+    "ats4" to SongInfo("Autumn Song 4", "Artist H", 2020),
+    "ats5" to SongInfo("Autumn Song 5", "Artist H", 2020),
     "autumn_music_6" to SongInfo("Autumn Song 6", "Artist H", 2020),
     "winter_music_1" to SongInfo("Winter Song 1", "Artist I", 2020),
     "winter_music_2" to SongInfo("Winter Song 2", "Artist I", 2020),
